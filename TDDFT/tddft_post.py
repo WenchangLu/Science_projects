9dc710e27f2e7843dfe2b34174794c4f1f30bdf7
WenchangLu <wlu2@ncsu.edu>
2025-06-13 11:39:17 -0400

    modify th tddft_post.py

--- TDDFT/tddft_post.py
+++ TDDFT/tddft_post.py
@@ -5,6 +5,7 @@ import os
 import math
 import numpy
 import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter1d
 
 if len(sys.argv) >1:
     f = open(sys.argv[1], 'r')
@@ -20,27 +21,25 @@ else:
     fout = open('spectra.dat', 'w')
     
 energy_range = 10.0
+delta_e = 0.001
+gauss_sigma = 20
 #one_direction = 1, only count the dipole of efield direction 
 #              = 0: average 3 directions
 one_direction = 1
 
 
 # maximum steps to be postprocesing
-n_data_max = 1000
+n_data_max = 20000
 #interpolation of energy points
-ndup = 1
-
-# nsmooth = 2: no smooth
-nsmooth = 2
 
 all_lines = f.readlines()
 f.close
 
 line = all_lines[1].split()
-efield_x = abs(float(line[2]))
-efield_y = abs(float(line[3]))
-efield_z = abs(float(line[4]))
-  
+efield_x = abs(float(line[5]))
+efield_y = abs(float(line[6]))
+efield_z = abs(float(line[7]))
+print("efiled", efield_x, efield_y, efield_z)
 if efield_x > 0.0: 
     if efield_y > 0.0: print ('warning: both x and y field >0.0')
     if efield_z > 0.0: print ('warning: both x and z field >0.0')
@@ -54,37 +53,35 @@ else:
     print ('all x, y, z field == 0')
 
 efield = max(efield_x, efield_y, efield_z)
-  
-      
-n_data = min(len(all_lines) -3, n_data_max)
 
-dipole = numpy.zeros(n_data)
-t = numpy.zeros(n_data)
+dt = float(all_lines[4].split()[0])-float(all_lines[3].split()[0])
+period = 2.0 * 3.1415926/delta_e * 27.2114
+
+n_data_tot = int(period/dt)
+print(n_data_tot)
+n_data = min(len(all_lines)-3, n_data_max)
+dipole = numpy.zeros(n_data_tot)
 
 for i in range(n_data):
     line = all_lines[i + 3].split()
-    t[i] = float(line[0])
     if(one_direction):
         dipole[i] = float(line[col_pick]) 
     else:
         dipole[i] = float(line[1]) +float(line[2]) + float(line[3])
 
       
-mean_dipole =  numpy.mean(dipole)
+mean_dipole =  numpy.mean(dipole[0:n_data])
 
 for i in range(n_data):
     dipole[i] = (dipole[i] - mean_dipole) 
     
 
-fw = numpy.fft.rfft(dipole, n_data)
+fw = numpy.fft.rfft(dipole, n_data_tot)
   
-num_freq = n_data//2 +1
-dt = t[1] - t[0]
-period = n_data * dt
-dw = 2.0 * 3.1415926/period * 27.2114
+num_freq = n_data_tot//2 +1
 
 wmin = 0.0
-wmax = num_freq * dw
+wmax = num_freq * delta_e
 x = numpy.linspace(wmin, wmax, num_freq)
 
 
@@ -105,13 +102,7 @@ for i in range(len(fw)):
         spectrum_0[i] = fw[i].imag/efield
         num_epoint +=1
 spectrum_0.resize(num_epoint)
-fw1 = numpy.fft.fft(spectrum_0, num_epoint)
-fw2 = numpy.zeros(num_epoint *ndup) + 0.0* 1j
-fw2[0] = fw1[0]
-for i in range(1, len(fw1)//nsmooth+1):
-    fw2[i] = fw1[i]
-    fw2[num_epoint*ndup-i] = fw1[num_epoint -i]
-fw = numpy.fft.ifft(fw2, num_epoint * ndup)
-for i in range(len(fw)):
-    fout.write("%f  %f\n"%(i*dw/ndup, (ndup * fw[i].real/n_data)))
+spectrum_1 = gaussian_filter1d(spectrum_0, gauss_sigma)
+for i in range(num_epoint):
+    fout.write("%f  %f\n"%(i*delta_e, spectrum_1[i]))
 fout.write("&\n")
